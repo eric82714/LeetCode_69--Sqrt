cf3aaf54facb75b0fce8cafece752fd772c5aec6
eric82714 <0958820714ab@gmail.com>
2019-12-20 17:15:08 +0800

    Create sqrt.cpp

---
+++ C++/sqrt.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0) return 0;
+        else if(x == 1) return 1;
+        else { 
+            double result = x / 2;
+        
+            while(result * result > x + 0.1) {
+                result = (result + x / result) / 2;
+            }
+            return (int)result;
+        }
+    }
+};
